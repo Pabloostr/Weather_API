b5ba6f9712ee92dbc9f74c6328723c892e658772
Pavlo Ostrozhynskyi <ikolochko22@gmail.com>
2021-06-22 16:31:08 +0300

    add apiKey

--- Weather_API/ViewController.swift
+++ Weather_API/ViewController.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import NVActivityIndicatorView
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     
     @IBOutlet weak var locationLabel: UILabel!
@@ -18,10 +22,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var backgroundView: UIView!
     
     let gradientLayer = CAGradientLayer()
-    
+    let apiKey = "c9117d6d0a018488deacd14ae0f08971"
+    let lat = 11.344533
+    let lon = 104.33322
+    var activityIndicator: NVActivityIndicatorView!
+    let locationManager = CLLocationManager()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         backgroundView.layer.addSublayer(gradientLayer)
+        let indicatorSize: CGFloat = 70
+        let indicatorFrame = CGRect(x: (view.frame.width-indicatorSize)/2, y: (view.frame.height-indicatorSize)/2, width: indicatorSize, height: indicatorSize)
+        activityIndicator = NVActivityIndicatorView(frame: indicatorFrame, type: .lineScale, color: UIColor.white, padding: 20.0)
+        activityIndicator.backgroundColor = UIColor.black
+        view.addSubview(activityIndicator)
+        
+        locationManager.requestWhenInUseAuthorization()
+        
+        activityIndicator.startAnimating()
+        if(CLLocationManager.locationServicesEnabled()) {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
