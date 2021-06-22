a1f57818ebd37d0b891ae4f053200d85e0ebe9a1
Pavlo Ostrozhynskyi <ikolochko22@gmail.com>
2021-06-22 17:19:35 +0300

    all done

--- Weather_API/ViewController.swift
+++ Weather_API/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Pavlo Ostrozhynskyi on 22.06.2021.
 //
 
+import Foundation
 import UIKit
 import Alamofire
 import SwiftyJSON
@@ -23,8 +24,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     let gradientLayer = CAGradientLayer()
     let apiKey = "c9117d6d0a018488deacd14ae0f08971"
-    let lat = 11.344533
-    let lon = 104.33322
+    var lat = 11.344533
+    var lon = 104.33322
     var activityIndicator: NVActivityIndicatorView!
     let locationManager = CLLocationManager()
         
@@ -51,6 +52,40 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         setBlueGradientBackground()
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        lat = location.coordinate.latitude
+        lon = location.coordinate.longitude
+        Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(apiKey)&units=metric").responseJSON {
+            response in
+            self.activityIndicator.stopAnimating()
+            if let responseStr = response.result.value {
+                let jsonResponse = JSON(responseStr)
+                let jsonWeather = jsonResponse["weather"].array![0]
+                let jsonTemp = jsonResponse["main"]
+                let iconName = jsonWeather["icon"].stringValue
+                
+                self.locationLabel.text = jsonResponse["name"].stringValue
+                self.conditionImageView.image = UIImage(named: iconName)
+                self.conditionLabel.text = jsonWeather["main"].stringValue
+                self.temperatureLabel.text = "\(Int(round(jsonTemp["temp"].doubleValue)))"
+                
+                let date = Date()
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "EEEE"
+                self.dayLabel.text = dateFormatter.string(from: date)
+                
+                let suffix = iconName.suffix(1)
+                if (suffix == "n") {
+                    self.setGrayGradientBackground()
+                }else {
+                    self.setBlueGradientBackground()
+                }
+            }
+        }
+        self.locationManager.stopUpdatingLocation()
+    }
+    
     func setBlueGradientBackground() {
         let topColor = UIColor(red: 95.0/255.0, green: 165.0/255.0, blue: 1.0, alpha: 1.0).cgColor
         let bottomColor = UIColor(red: 72.0/255.0, green: 114.0/255.0, blue: 184.0/255.0, alpha: 1.0).cgColor
